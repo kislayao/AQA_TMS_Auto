da91af2a92fa39334b181076388725d4dc77933d
kislayao <olya.drozd.al@gmail.com>
2023-06-30 22:05:27 +0200

    final changes commit

--- src/test/java/CalculatorTest_Div_Double.java
+++ src/test/java/CalculatorTest_Div_Double.java
@@ -1,8 +1,13 @@
 import data.StaticProvider;
 import org.testng.Assert;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 public class CalculatorTest_Div_Double extends BaseTest{
+
+    private double a = 3.0;
+    private double b = 1.0;
 
     @Test (groups = "smoke", dataProvider = "Data for div DOUBLE test", dataProviderClass = StaticProvider.class)
     public void dataProviderTest_DivDouble(double a, double b, double expectedResult) {
@@ -23,10 +28,32 @@ public class CalculatorTest_Div_Double extends BaseTest{
     }
 
     @Test(groups = "regression")
-    public void divideNan() {
+    public void divideDoubleZeroByZero() {
         double actualValue = calculator.div(0.0,0);
 
         Assert.assertEquals(actualValue, Double.NaN);
+    }
+
+    @Parameters({"firstDigit-value","secondDigit-value"})
+    @Test
+    public void parametersTest(@Optional("2.4") double firstDigit,  @Optional("0.6") double secondDigit){
+        double expectedValue = 4.0;
+        double actualValue = calculator.div(firstDigit,secondDigit);
+
+        Assert.assertEquals(actualValue, expectedValue, "Values don't match");
+    }
+
+    @Test (retryAnalyzer = Retry.class)
+    public void retryDivDouble(){
+        double expectedValue = 2.0;
+
+        if (calculator.div(a,b) == expectedValue){
+            Assert.assertTrue(true);
+        } else {
+            a++;
+            b++;
+            Assert.assertTrue(false);
+        }
     }
 
 }
